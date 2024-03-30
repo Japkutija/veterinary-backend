2e20535c824bbb09e4e00d72e03bac54364f7983
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-03-30 13:13:46 +0100

    - Initial commit

---
+++ src/main/java/com/Japkutija/veterinarybackend/VeterinaryClinicBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Japkutija.veterinarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryClinicBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryClinicBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Japkutija/veterinarybackend/VeterinaryClinicBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Japkutija.veterinarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
