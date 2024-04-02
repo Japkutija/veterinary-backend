f9bccc7d59fc80423579745330ff3510063b1f2f
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-02 13:59:11 +0200

    - Setup Service Layer Interfaces and Implementations.

---
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/service/impl/InventoryServiceImpl.java
@@ -0,0 +1,7 @@
+package com.Japkutija.veterinarybackend.veterinary.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryServiceImpl implements com.Japkutija.veterinarybackend.veterinary.service.InventoryService{
+}
