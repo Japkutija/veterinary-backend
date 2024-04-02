b1147b23214e1729805d65db1acabf5cf7ce9f81
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-02 13:59:11 +0200

    - Setup Service Layer Interfaces and Implementations.

---
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/service/impl/AppointmentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.Japkutija.veterinarybackend.veterinary.service.impl;
+
+import com.Japkutija.veterinarybackend.veterinary.service.AppointmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentServiceImpl implements AppointmentService {
+}
---
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/service/impl/SpeciesServiceImpl.java
@@ -0,0 +1,7 @@
+package com.Japkutija.veterinarybackend.veterinary.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpeciesServiceImpl implements com.Japkutija.veterinarybackend.veterinary.service.SpeciesService{
+}
