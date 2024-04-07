e1d0590eaecc1c81beda4a7b8c9c71540d98d355
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-07 17:09:49 +0200

    - Added conversion methods to BreedMapper for mapping between Breed entity and BreedDTO.

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/mapper/BreedMapper.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/mapper/BreedMapper.java
@@ -1,4 +1,27 @@
 package com.Japkutija.veterinarybackend.veterinary.mapper;
 
+import com.Japkutija.veterinarybackend.veterinary.model.dto.BreedDTO;
+import com.Japkutija.veterinarybackend.veterinary.model.entity.Breed;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface BreedMapper {
+
+    @Mapping(source = "speciesUuid", target = "species.uuid")
+    Breed toBreed(BreedDTO breedDTO);
+
+    @Mapping(source = "species.uuid", target = "speciesUuid")
+    BreedDTO toBreedDto(Breed breed);
+
+    List<Breed> toBreedList(List<BreedDTO> breedDTOs);
+
+    List<BreedDTO> toBreedDTOList(List<Breed> breeds);
+
+    Breed updateBreedFromDto(BreedDTO breedDTO, @MappingTarget Breed breed);
+
+
 }
