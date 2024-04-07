bf54f927da489eb6ab0bed986bcae4b17b3e6984
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-07 17:40:17 +0200

    - Added mapping methods in MedicalRecordMapper.

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/mapper/MedicalRecordMapper.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/mapper/MedicalRecordMapper.java
@@ -1,4 +1,25 @@
 package com.Japkutija.veterinarybackend.veterinary.mapper;
 
+import com.Japkutija.veterinarybackend.veterinary.model.dto.MedicalRecordDTO;
+import com.Japkutija.veterinarybackend.veterinary.model.entity.MedicalRecord;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface MedicalRecordMapper {
+
+    MedicalRecord toMedicalRecord(MedicalRecordDTO medicalRecordDTO);
+
+    MedicalRecordDTO toMedicalRecordDTO(MedicalRecord medicalRecord);
+
+    List<MedicalRecord> toMedicalRecordList(List<MedicalRecordDTO> medicalRecordDTOs);
+
+    List<MedicalRecordDTO> toMedicalRecordDTOList(List<MedicalRecord> medicalRecords);
+
+    
+    MedicalRecord updateMedicalRecordFromDto(MedicalRecordDTO medicalRecordDTO, @MappingTarget MedicalRecord medicalRecord);
 }
+
