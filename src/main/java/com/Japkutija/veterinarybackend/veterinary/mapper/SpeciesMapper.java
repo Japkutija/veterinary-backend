987daea61b92080ceaeeae3581e1218184f75266
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-07 18:49:45 +0200

    - Added SpeciesMapper for converting between Species entity and SpeciesDTO. - Implemented methods for single and list conversions.

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/mapper/SpeciesMapper.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/mapper/SpeciesMapper.java
@@ -1,4 +1,20 @@
 package com.Japkutija.veterinarybackend.veterinary.mapper;
 
+import com.Japkutija.veterinarybackend.veterinary.model.dto.SpeciesDTO;
+import com.Japkutija.veterinarybackend.veterinary.model.entity.Species;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface SpeciesMapper {
+
+    Species toSpecies(SpeciesDTO speciesDTO);
+
+    SpeciesDTO toSpeciesDTO(Species species);
+
+    List<Species> toSpeciesList(List<SpeciesDTO> speciesDTOs);
+
+    List<SpeciesDTO> toSpeciesDTOList(List<Species> species);
 }
+
