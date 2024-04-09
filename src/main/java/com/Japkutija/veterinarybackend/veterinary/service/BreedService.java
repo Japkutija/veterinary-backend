587f10dc61daf82033d7724df9b1da36b16e541b
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-09 18:39:20 +0200

    - Implemented full CRUD operations in BreedService - Integrated BreedMapper for DTO-to-entity conversions and vice versa - Added methods for breed retrieval by UUID and species UUID - Ensured error handling with custom exceptions for clearer issue identification - Adjusted EntitySavingException parameter to Breed.class in saveBreed method - Reviewed service for adherence to best practices in readability and maintainability

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/service/BreedService.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/service/BreedService.java
@@ -1,4 +1,28 @@
 package com.Japkutija.veterinarybackend.veterinary.service;
 
+import com.Japkutija.veterinarybackend.veterinary.model.dto.BreedDTO;
+import com.Japkutija.veterinarybackend.veterinary.model.entity.Breed;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface BreedService {
+
+    Breed createBreed(BreedDTO breedDTO);
+
+    Breed saveBreed(Breed breed);
+
+    Breed getBreedByUuid(UUID uuid);
+
+
+    List<Breed> getAllBreeds();
+
+    List<Breed> getBreedsBySpeciesUuid(UUID speciesUuid);
+
+
+    Breed updateBreed(UUID uuid, BreedDTO breedDTO);
+
+    void deleteBreed(UUID uuid);
+
+
 }
--- src/main/java/com/Japkutija/veterinarybackend/veterinary/service/impl/BreedService.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/service/impl/BreedService.java
@@ -1,7 +1,90 @@
 package com.Japkutija.veterinarybackend.veterinary.service.impl;
 
+import com.Japkutija.veterinarybackend.veterinary.exception.EntityNotFoundException;
+import com.Japkutija.veterinarybackend.veterinary.mapper.BreedMapper;
+import com.Japkutija.veterinarybackend.veterinary.model.dto.BreedDTO;
+import com.Japkutija.veterinarybackend.veterinary.model.entity.Bill;
+import com.Japkutija.veterinarybackend.veterinary.model.entity.Breed;
+import com.Japkutija.veterinarybackend.veterinary.repository.BreedRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class BreedService implements com.Japkutija.veterinarybackend.veterinary.service.BreedService {
+
+    private final BreedRepository breedRepository;
+    private final BreedMapper breedMapper;
+
+    @Override
+    public Breed createBreed(BreedDTO breedDTO) {
+        var breed = breedMapper.toBreed(breedDTO);
+
+        return saveBreed(breed);
+    }
+
+    @Override
+    public Breed saveBreed(Breed breed) {
+
+        try {
+            return breedRepository.save(breed);
+        } catch (Exception ex) {
+            log.error("Error saving breed: {}", ex.getMessage());
+            throw new EntitySavingException(Breed.class, ex);
+        }
+
+    }
+
+    @Override
+    public Breed getBreedByUuid(UUID uuid) {
+        var breed = breedRepository.findByUuid(uuid);
+
+        return breed.orElseThrow(() -> new EntityNotFoundException(Breed.class, uuid));
+    }
+
+    @Override
+    public List<Breed> getAllBreeds() {
+        var breeds = breedRepository.findAll();
+
+        if (breeds.isEmpty()) {
+            return List.of();
+        }
+        return breeds;
+    }
+
+    @Override
+    public List<Breed> getBreedsBySpeciesUuid(UUID speciesUuid) {
+        var breeds = breedRepository.findAllBySpeciesUuid(speciesUuid);
+
+        if (breeds.isEmpty()) {
+            return List.of();
+        }
+
+        return breeds;
+    }
+
+    @Override
+    public Breed updateBreed(UUID uuid, BreedDTO breedDTO) {
+
+        var breed = getBreedByUuid(uuid);
+
+        breedMapper.updateBreedFromDto(breedDTO, breed);
+
+        return saveBreed(breed);
+
+    }
+
+    @Override
+    public void deleteBreed(UUID uuid) {
+
+        var breed = getBreedByUuid(uuid);
+
+        breedRepository.delete(breed);
+
+    }
 }
