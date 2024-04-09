f1032689c1a11d038dfc134af7ef5c61d4a91aa2
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-09 18:39:20 +0200

    - Implemented full CRUD operations in BreedService - Integrated BreedMapper for DTO-to-entity conversions and vice versa - Added methods for breed retrieval by UUID and species UUID - Ensured error handling with custom exceptions for clearer issue identification - Adjusted EntitySavingException parameter to Breed.class in saveBreed method - Reviewed service for adherence to best practices in readability and maintainability

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/repository/BreedRepository.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/repository/BreedRepository.java
@@ -4,6 +4,13 @@ import com.Japkutija.veterinarybackend.veterinary.model.entity.Breed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface BreedRepository extends JpaRepository<Breed, Long> {
+    Optional<Breed> findByUuid(UUID uuid);
+
+    List<Breed> findAllBySpeciesUuid(UUID speciesUuid);
 }
