a3becc7f71c24e8f47e0292905903d2d1bf9b360
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-11 13:03:28 +0200

    - Implemented CRUD operations in TreatmentServiceImpl. - Utilized @Transactional annotation for appropriate data integrity. - Integrated TreatmentMapper for DTO-entity conversions. - Added methods for creating, updating, retrieving, and deleting treatments. - Ensured exception handling for entity saving and not found scenarios.

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/repository/TreatmentRepository.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/repository/TreatmentRepository.java
@@ -4,6 +4,10 @@ import com.Japkutija.veterinarybackend.veterinary.model.entity.Treatment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface TreatmentRepository extends JpaRepository<Treatment, Long> {
+    Optional<Treatment> findByUuid(UUID treatmentUUID);
 }
