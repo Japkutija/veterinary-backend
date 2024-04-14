ad4c6f3a0a80a58b74edcedd5d0fa5bc7be5f7bb
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-04-14 16:13:47 +0200

    - Added endpoints for creating, updating, fetching, and deleting appointments. - Included detailed Swagger API annotations for clear API documentation. - Implemented validation constraints in AppointmentDTO to ensure robust data handling. - Routes are clearly defined for operations based on UUIDs for appointments, owners, and pets to maintain a coherent and navigable API structure.

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/model/dto/AppointmentDTO.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/model/dto/AppointmentDTO.java
@@ -1,6 +1,10 @@
 package com.Japkutija.veterinarybackend.veterinary.model.dto;
 
 import com.Japkutija.veterinarybackend.veterinary.model.enums.AppointmentStatus;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.Instant;
@@ -10,12 +14,29 @@ import java.util.UUID;
 @Data
 public class AppointmentDTO {
 
+    @NotNull(message = "UUID is required")
     private UUID uuid;
+
+    @NotNull(message = "Appointment date is required")
+    @FutureOrPresent(message = "Appointment date must be in the present or future")
     private Date appointmentDate;
+
+    @NotNull(message = "Appointment time is required")
     private Instant appointmentTime;
+
+    @NotNull(message = "Reason is required")
+    @Size(min = 10, max = 255, message = "Reason must be between 10 and 255 characters")
     private String reason;
+
+    @NotNull(message = "Appointment status is required")
     private AppointmentStatus status;
+
+    @NotNull(message = "Pet UUID is required")
     private UUID petUuid;
+
+    @NotNull(message = "Owner UUID is required")
     private UUID ownerUuid;
+
+    @NotNull(message = "Bill UUID is required")
     private UUID billUuid;
 }
