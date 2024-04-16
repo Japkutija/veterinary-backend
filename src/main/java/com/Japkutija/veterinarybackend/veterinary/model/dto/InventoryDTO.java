995e7d1957f2670c27ec8be37043be1881ba9cf2
Jasmin Dervisevic <jasmin.dervisevic@endava.com>
2024-04-16 13:30:32 +0200

    - Implemented CRUD operations in InventoryController with full Swagger annotations for API documentation. - Added comprehensive validation constraints in InventoryDTO to ensure robust data handling. - Ensured proper use of HTTP status codes and responses for RESTful service interaction. - Added error handling for non-existent resources and invalid inputs.

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/model/dto/InventoryDTO.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/model/dto/InventoryDTO.java
@@ -1,5 +1,7 @@
 package com.Japkutija.veterinarybackend.veterinary.model.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
@@ -7,9 +9,19 @@ import java.util.UUID;
 @Data
 public class InventoryDTO {
 
+    @NotNull(message = "Inventory UUID is required")
     private UUID uuid;
+
+    @NotNull(message = "Item name is required")
     private String itemName;
+
+    @NotNull(message = "Item description is required")
     private String itemDescription;
+
+    @NotNull(message = "Quantity in stock is required")
+    @Min(value = 0, message = "Quantity in stock must be greater than or equal to 0")
     private int quantityInStock;
+
+    @NotNull(message = "Reorder level is required")
     private int reorderLevel;
 }
