f8aa88d72dfab98ed96a6d7869c42b86056ccf41
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-08-22 23:44:41 +0200

    - Implemented JWT-based authentication and authorization, - Added login endpoint for user authentication, - Enhanced security configuration by integrating JWT-based authorization, - Implemented custom user details service for authentication and authorization, - Added dependencies for JWT token handling

--- src/main/java/com/Japkutija/veterinarybackend/veterinary/controller/AuthController.java
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/controller/AuthController.java
@@ -1,16 +1,20 @@
 package com.Japkutija.veterinarybackend.veterinary.controller;
 
 import com.Japkutija.veterinarybackend.veterinary.mapper.UserMapper;
-import com.Japkutija.veterinarybackend.veterinary.model.dto.UserDto;
+import com.Japkutija.veterinarybackend.veterinary.model.dto.request.AuthenticationRequest;
 import com.Japkutija.veterinarybackend.veterinary.model.dto.request.UserRegistrationDto;
+import com.Japkutija.veterinarybackend.veterinary.model.dto.response.AuthenticationResponse;
 import com.Japkutija.veterinarybackend.veterinary.model.dto.response.ErrorResponseDto;
+import com.Japkutija.veterinarybackend.veterinary.security.CustomUserDetailsService;
+import com.Japkutija.veterinarybackend.veterinary.security.util.JwtUtil;
 import com.Japkutija.veterinarybackend.veterinary.service.impl.UserServiceImpl;
 import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService customUserDetailsService;
     private final UserServiceImpl userService;
     private final UserMapper userMapper;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegistrationDto registrationDto) {
@@ -43,5 +51,24 @@ public class AuthController {
         var registeredUserDto = userMapper.toUserDto(registeredUser);
 
         return ResponseEntity.ok(registeredUserDto);
+    }
+
+    /**
+     * Handles the login request and generates a JWT token upon successful authentication.
+     *
+     * @param authRequest the authentication request containing username and password
+     * @return a ResponseEntity containing the authentication response with the JWT token
+     * @throws Exception if authentication fails
+     */
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> createAuthenticationToken(@RequestBody AuthenticationRequest authRequest) throws Exception {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+
+        final var userDetails = customUserDetailsService.loadUserByUsername(authRequest.getUsername());
+        final var jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
---
+++ src/main/java/com/Japkutija/veterinarybackend/veterinary/security/JwtRequestFilter.java
@@ -0,0 +1,56 @@
+package com.Japkutija.veterinarybackend.veterinary.security;
+
+import com.Japkutija.veterinarybackend.veterinary.security.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        final var authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            try {
+                username = jwtUtil.extractUsername(jwt);
+            } catch (ExpiredJwtException e) {
+                log.error("JWT Token has expired");
+            }
+        }
+
+        // Validate token and set authentication
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = this.userDetailsService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, userDetails.getUsername())) {
+                var authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
